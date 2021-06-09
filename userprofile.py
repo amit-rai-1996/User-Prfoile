726ee2394e8cb0cedef6954a0daf8907b4ab75f2
Amit Rai <amitrai220@gmail.com>
2021-06-09 19:40:37 +0530

    Just a pyhton programme.

---
+++ userprofile.py
@@ -0,0 +1,34 @@
+# User-Prfoile
+#First Python Programme.
+print("Hey what's your name !!")
+
+name =input()
+print ("Nice to meet you", name, "." "Hope you are doing well.")
+print(type(name))
+
+print("Can you tell me your contact Number ?")
+contact = int(input())
+print ("Hey", "thanks for your contact number -", contact)
+print(type(contact))
+
+print("What's your age?")
+age = int(input())
+print("You look Younnger than", age)
+print(type(age))
+
+print("Are you on social media plarforms ? Name them.")
+socialmedia = input()
+print("So you're active on", socialmedia, "that's great !!")
+print(type(socialmedia))
+
+print("Where do you live?")
+city = input()
+print("So you live in", city, "that's an amazing place to live.....")
+print(type(city))
+
+print("Tell me what you do in your spare time ?")
+hobbies = input()
+print("So you like", hobbies, "in your spare time!!! that sounds great!! ")
+print(type(hobbies))
+
+print("Thank you", name, "for giving your valuable time and information.", " :)")
